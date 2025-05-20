35a16641d185ff644a063cac30aa0827c2696912
DevVugar <rehimovvuqarrrr@gmail.com>
2025-05-20 21:55:20 +0400

    first commit

---
+++ src/main/java/com/example/springtask/SpringTaskApplication.java
@@ -0,0 +1,15 @@
+package com.example.springtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTaskApplication.class, args);
+
+        System.out.println("my project is running...");
+    }
+
+}
